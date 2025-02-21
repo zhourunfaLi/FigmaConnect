c113731049d8b996dbc6bb8ee7ac18280a45a315
lhmclickhere <39524676-lhmclickhere@users.noreply.replit.com>
2025-02-21 03:45:21 +0000

    User checkpoint: Update Repl configuration and server port to 8080 to resolve run repl issue.

--- server/index.ts
+++ server/index.ts
@@ -56,9 +56,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client
-  const PORT = 5000;
+  // Try different ports if the default one is in use
+  const PORT = 8080;
   server.listen(PORT, "0.0.0.0", () => {
     log(`serving on port ${PORT}`);
   });
